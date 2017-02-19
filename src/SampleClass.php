f0aff406730d6ff9c65eee44842648c5abeb8982
shayan <shayanadc@gmail.com>
2017-02-19 11:54:26 +0330

    add pack name

--- src/SampleClass.php
+++ src/SampleClass.php
@@ -1,13 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: faramarz
- * Date: 1/16/17
- * Time: 12:57
- */
-
-namespace FSalehpour\PHPPackageSkeleton;
-
 
 class SampleClass
 {
--- tests/SampleClassTest.php
+++ tests/SampleClassTest.php
@@ -1,14 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: faramarz
- * Date: 1/16/17
- * Time: 13:15
- */
-
-
-use FSalehpour\PHPPackageSkeleton\SampleClass;
-
 
 class SampleClassTest extends PHPUnit_Framework_TestCase
 {
