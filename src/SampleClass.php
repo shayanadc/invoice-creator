018401abb02c272a40aaf44f7dd9fb3e71169e53
shayan <shayanadc@gmail.com>
2017-02-19 11:50:45 +0330

    first commit

---
+++ src/SampleClass.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: faramarz
+ * Date: 1/16/17
+ * Time: 12:57
+ */
+
+namespace FSalehpour\PHPPackageSkeleton;
+
+
+class SampleClass
+{
+
+}
---
+++ tests/SampleClassTest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: faramarz
+ * Date: 1/16/17
+ * Time: 13:15
+ */
+
+
+use FSalehpour\PHPPackageSkeleton\SampleClass;
+
+
+class SampleClassTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function itReturnsTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
